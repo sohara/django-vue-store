6d2a1d309698889410158eecddfa795f8b715bc4
Sean O'Hara <sohara@sohara.com>
2024-04-17 16:23:28 -0400

    Add Header and Footer components for main layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <Header />
 
   <RouterView />
+  <FooterComponent />
 </template>
