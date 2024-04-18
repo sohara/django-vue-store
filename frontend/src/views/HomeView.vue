fcd6ce58d45bbe10033c29ea9a27bd86638b5882
Sean O'Hara <sohara@sohara.com>
2024-04-18 17:13:45 -0400

    Add product detail page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -17,10 +17,10 @@
           <div class="mt-4 flex justify-between">
             <div>
               <h3 class="text-sm text-gray-700">
-                <a :href="product.get_absolute_url">
+                <RouterLink v-bind:to="product.get_absolute_url">
                   <span aria-hidden="true" class="absolute inset-0" />
                   {{ product.name }}
-                </a>
+                </RouterLink>
               </h3>
               <p class="mt-1 text-sm text-gray-500">{{ product.description }}</p>
             </div>
